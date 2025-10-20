55b8e9ead6b2941362c2955668ee4ce431d0aa5f
Kosuri Indu <kosurilindu@gmail.com>
2025-10-20 21:26:45 +0530

    basic setup

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.route('/')
+def home():
+    return "Welcome to the BillBot!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
